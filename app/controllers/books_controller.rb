cf57bf92fb03ddf428256834ad595129cc084b54
tgaff <tgaff@alumni.nd.edu>
2015-09-02 20:39:30 -0700

    add methods to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
-  def new
-  end
 
   def index
+
   end
 
   def show
+
+  end
+
+  def create
+
   end
 
   def edit
+
   end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def book_params
+
+  end
+
 end
