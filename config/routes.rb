887fb101e055a91ee933d3ac59487145e10ae7d0
tgaff <tgaff@alumni.nd.edu>
2015-09-02 20:37:55 -0700

    change the routes for books to user resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-
-  get 'books/index'
-
-  get 'books/show'
-
-  get 'books/edit'
-
+  resources 'books'
   get 'static_pages/home'
 
   root 'static_pages#home'
