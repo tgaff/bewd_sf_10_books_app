b2a2d7cb97799bf39540bf6010f3280e0450d9cb
tgaff <tgaff@alumni.nd.edu>
2015-09-09 20:39:49 -0700

    setup the create method to save @book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,13 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
 
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
   end
 
   def edit
